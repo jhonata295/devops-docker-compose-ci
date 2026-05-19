b479be42820b43a3b1d2b9a9d2ef7296f01fa5d6
unknown <2414290158@ACADEMICO.IESB.BR>
2026-05-19 10:59:00 -0300

    fix: configurando frontend para acesso externo no docker

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,7 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-
 function App() {
-  const [message, setMessage] = useState('Carregando API...')
-
-  useEffect(() => {
-    axios
-      .get(import.meta.env.VITE_API_URL)
-      .then((response) => {
-        setMessage(response.data.message)
-      })
-      .catch(() => {
-        setMessage('Erro ao conectar API')
-      })
-  }, [])
-
   return (
     <div>
-      <h1>Projeto DevOps</h1>
-      <h2>{message}</h2>
+      <h1>DevOps Docker Compose CI/CD</h1>
     </div>
   )
 }
